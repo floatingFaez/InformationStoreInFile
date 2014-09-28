d2554d9ce13c4d9fcfea9a1642bdee0002584db3
floatingFaez <floatingfaez@live.com>
2014-09-28 21:08:06 +0600

    Information Store Whole Project Initially Uploaded

---
+++ emploeeInformation/StudentStoreApp.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace StudentStoreApp
+{
+    partial class StudentStoreApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.enterNameLabel = new System.Windows.Forms.Label();
+            this.fileNameTextBox = new System.Windows.Forms.TextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.showButton = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // enterNameLabel
+            // 
+            this.enterNameLabel.AutoSize = true;
+            this.enterNameLabel.Location = new System.Drawing.Point(45, 18);
+            this.enterNameLabel.Name = "enterNameLabel";
+            this.enterNameLabel.Size = new System.Drawing.Size(63, 13);
+            this.enterNameLabel.TabIndex = 0;
+            this.enterNameLabel.Text = "Enter Name";
+            // 
+            // fileNameTextBox
+            // 
+            this.fileNameTextBox.Location = new System.Drawing.Point(137, 15);
+            this.fileNameTextBox.Name = "fileNameTextBox";
+            this.fileNameTextBox.Size = new System.Drawing.Size(271, 20);
+            this.fileNameTextBox.TabIndex = 1;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(333, 41);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 2;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(333, 83);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(75, 23);
+            this.showButton.TabIndex = 2;
+            this.showButton.Text = "Show";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(48, 121);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(360, 147);
+            this.listBox1.TabIndex = 3;
+            // 
+            // FileUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(468, 310);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.fileNameTextBox);
+            this.Controls.Add(this.enterNameLabel);
+            this.Name = "FileUI";
+            this.Text = "File Input Output Stream";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label enterNameLabel;
+        private System.Windows.Forms.TextBox fileNameTextBox;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ emploeeInformation/StudentStoreApp.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentStoreApp
+{
+    public partial class StudentStoreApp : Form
+    {
+        private string fileLocation = @"studentName.txt";
+        public StudentStoreApp()
+        {
+            InitializeComponent();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            FileStream aFileStream = new FileStream(fileLocation, FileMode.Append);
+            StreamWriter aStreamWriter = new StreamWriter(aFileStream);
+            aStreamWriter.Write(fileNameTextBox.Text);
+            aStreamWriter.WriteLine();
+            aStreamWriter.Close();
+            fileNameTextBox.Text = "";
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            FileStream aFileStream = new FileStream(fileLocation, FileMode.Open);
+            StreamReader aStreamReader = new StreamReader(aFileStream);
+            listBox1.Items.Clear();
+            while (!aStreamReader.EndOfStream)
+            {
+                
+                string aLine = aStreamReader.ReadLine();
+                listBox1.Items.Add(aLine);
+
+                //textBox2.Text = textBox2.Text + aLine + "\n";
+            }
+            
+            aStreamReader.Close();
+        }
+
+    }
+}
